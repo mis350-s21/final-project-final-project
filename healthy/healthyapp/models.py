b5f86adae0e11d77c7ba921a994740a48c602cf1
Sara Alhajri <93153872+ssh218@users.noreply.github.com>
2022-01-06 14:47:32 +0300

    productsmodels

--- healthy/healthyapp/models.py
+++ healthy/healthyapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    item_name=models.CharField(max_length=100)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.CharField(max_length=300)
+    # notes=models.TextField()
+
+    def __str__(self):
+        return f"{self.item_name}"
