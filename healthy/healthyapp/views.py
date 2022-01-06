80bc48cd58b06c03f9a695903c0140c1d8e3b719
Sara Alhajri <93153872+ssh218@users.noreply.github.com>
2022-01-06 14:26:29 +0300

    Delete healthy directory

--- healthy/healthyapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Order
-# Create your views here.
-
-def order_list(request):
-    posts = Order.objects.all()
-    c = {'order_list': Order,}
-    return render(request, 'order_list.html', c)
