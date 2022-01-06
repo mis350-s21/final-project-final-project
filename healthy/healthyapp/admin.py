3d271328dfb192c9293676ce141d870c951390cd
Sara Alhajri <93153872+ssh218@users.noreply.github.com>
2022-01-06 15:11:33 +0300

    changes

--- healthy/healthyapp/admin.py
+++ healthy/healthyapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from.models import Products
 
 # Register your models here.
+class ProductsAdmin(admin.ModelAdmin):
+    list_display=('item_name', 'price', 'description', 'image')
+    search_fields = ['item_name',]
+admin.site.register(Products, ProductsAdmin)
