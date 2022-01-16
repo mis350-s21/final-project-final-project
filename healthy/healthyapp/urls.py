1abeae56071e64259940010c1c818d638d1e3ae5
zainabaltahoo <zainab_i1999@hotmail.com>
2022-01-17 00:22:26 +0300

    change zainab

--- healthy/healthyapp/urls.py
+++ healthy/healthyapp/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.greeting, name='home'),
+    #path('product/<int:id>', views.product, name='product'),
     path('search/', views.search, name='search'),
     path('about/', views.about, name='about'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('contact/', views.contact, name='contact'),
+    path('register', views.register, name='register'),
+    path('login', views.login_request, name='login'),
+    path('logout', views.logout_request, name='logout'),
 ]
