68115abd905b83662df3d88fc9418b4abb1b5c6a
Sara Alhajri <93153872+ssh218@users.noreply.github.com>
2022-01-17 22:53:28 +0300

    changes

--- healthy/healthyapp/urls.py
+++ healthy/healthyapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('', views.greeting, name='home'),
+    path('product/<int:id>', views.product, name='product'),
     path('search/', views.search, name='search'),
     path('about/', views.about, name='about'),
     path('cart/', views.cart, name='cart'),
